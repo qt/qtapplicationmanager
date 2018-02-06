b5b9138b77feb3b39e81eb95ebac347ccd1e7e06
Robert Griebl <robert.griebl@pelagicore.com>
2018-02-06 16:22:55 +0100

    Update Pelagicore copyright headers to 2018
    Change-Id: I8d9278053ccdef5a57e51753aa6157941ebcee52
    Reviewed-by: Dominik Holland <dominik.holland@pelagicore.com>

--- config.tests/libarchive/main.cpp
+++ config.tests/libarchive/main.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2017 Pelagicore AG
+** Copyright (C) 2018 Pelagicore AG
 ** Copyright (C) 2016 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
--- src/window-lib/waylandcompositor.cpp
+++ src/window-lib/waylandcompositor.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2017 Pelagicore AG
+** Copyright (C) 2018 Pelagicore AG
 ** Copyright (C) 2016 Klarälvdalens Datakonsult AB, a KDAB Group company
 ** Contact: https://www.qt.io/licensing/
 **

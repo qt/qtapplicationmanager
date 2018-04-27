qt/qtapplicationmanager

commit d47e6e66df5b7b457319572334c981d38b6e32a6

Move sanity checks in the qml launcher
We tested the parameters coming from the AM without being sure that we were
being called from the AM at all (--help / --directload)

Change-Id: I3928701ef3981b7681174dd030f9b16d463aa512
Reviewed-by: Dominik Holland <dominik.holland@pelagicore.com>

src/launchers/qml/main.cpp
@@ -194,6 +194,7 @@ int main(int argc, char *argv[])
             }
             directLoad = fi.absoluteFilePath();
         } else {
+            a.loadConfiguration();
             a.setupDBusConnections();
             StartupTimer::instance()->checkpoint("after dbus initialization");
         }

src/launcher-lib/launchermain.cpp
@@ -53,47 +53,10 @@
 
 QT_BEGIN_NAMESPACE_AM
 
-LauncherMain::LauncherMain(int &argc, char **argv, const QByteArray &configYaml) Q_DECL_NOEXCEPT_EXPR(false)
+LauncherMain::LauncherMain(int &argc, char **argv) Q_DECL_NOEXCEPT
     : LauncherMainBase(SharedMain::preConstructor(argc), argv)
     , SharedMain()
-{
-    auto docs = QtYaml::variantDocumentsFromYaml(configYaml.isEmpty() ? qgetenv("AM_CONFIG")
-                                                                      : configYaml);
-    if (docs.size() == 1)
-        m_configuration = docs.first().toMap();
-
-    m_baseDir = m_configuration.value(qSL("baseDir")).toString() + qL1C('/');
-    m_runtimeConfiguration = m_configuration.value(qSL("runtimeConfiguration")).toMap();
-    m_securityToken = QByteArray::fromHex(m_configuration.value(qSL("")).toString().toLatin1());
-    m_systemProperties = m_configuration.value(qSL("systemProperties")).toMap();
-
-    QVariantMap loggingConfig = m_configuration.value(qSL("logging")).toMap();
-    m_loggingRules = variantToStringList(loggingConfig.value(qSL("rules")));
-
-    QVariantMap dbusConfig = m_configuration.value(qSL("dbus")).toMap();
-    m_dbusAddressP2P = dbusConfig.value(qSL("p2p")).toString();
-    m_dbusAddressNotifications = dbusConfig.value(qSL("org.freedesktop.Notifications")).toString();
-
-    QVariantMap uiConfig = m_configuration.value(qSL("ui")).toMap();
-    m_slowAnimations = uiConfig.value(qSL("slowAnimations")).toBool();
-    m_openGLConfiguration = uiConfig.value(qSL("opengl")).toMap();
-
-    // un-comment this if things go south:
-    //qWarning() << "### LOG " << m_loggingRules;
-    //qWarning() << "### DBUS" << dbusConfig;
-    //qWarning() << "### UI  " << uiConfig;
-    //qWarning() << "### RT  " << m_runtimeConfiguration;
-    //qWarning() << "### SYSP" << m_systemProperties;
-    //qWarning() << "### GL  " << m_openGLConfiguration;
-
-    // sanity checks
-    if (m_baseDir == qL1S("/"))
-        throw Exception("Runtime launcher received an empty baseDir");
-    if (loggingConfig.isEmpty())
-        throw Exception("Runtime launcher received no logging configuration");
-    if (dbusConfig.isEmpty())
-        throw Exception("Runtime launcher received no D-Bus configuration");
-}
+{ }
 
 LauncherMain::~LauncherMain()
 { }
@@ -141,6 +104,46 @@ QString LauncherMain::notificationDBusName() const
 QVariantMap LauncherMain::openGLConfiguration() const
 {
     return m_openGLConfiguration;
+}
+
+void LauncherMain::loadConfiguration(const QByteArray &configYaml) Q_DECL_NOEXCEPT_EXPR(false)
+{
+    auto docs = QtYaml::variantDocumentsFromYaml(configYaml.isEmpty() ? qgetenv("AM_CONFIG")
+                                                                      : configYaml);
+    if (docs.size() == 1)
+        m_configuration = docs.first().toMap();
+
+    m_baseDir = m_configuration.value(qSL("baseDir")).toString() + qL1C('/');
+    m_runtimeConfiguration = m_configuration.value(qSL("runtimeConfiguration")).toMap();
+    m_securityToken = QByteArray::fromHex(m_configuration.value(qSL("")).toString().toLatin1());
+    m_systemProperties = m_configuration.value(qSL("systemProperties")).toMap();
+
+    QVariantMap loggingConfig = m_configuration.value(qSL("logging")).toMap();
+    m_loggingRules = variantToStringList(loggingConfig.value(qSL("rules")));
+
+    QVariantMap dbusConfig = m_configuration.value(qSL("dbus")).toMap();
+    m_dbusAddressP2P = dbusConfig.value(qSL("p2p")).toString();
+    m_dbusAddressNotifications = dbusConfig.value(qSL("org.freedesktop.Notifications")).toString();
+
+    QVariantMap uiConfig = m_configuration.value(qSL("ui")).toMap();
+    m_slowAnimations = uiConfig.value(qSL("slowAnimations")).toBool();
+    m_openGLConfiguration = uiConfig.value(qSL("opengl")).toMap();
+
+    // un-comment this if things go south:
+    //qWarning() << "### LOG " << m_loggingRules;
+    //qWarning() << "### DBUS" << dbusConfig;
+    //qWarning() << "### UI  " << uiConfig;
+    //qWarning() << "### RT  " << m_runtimeConfiguration;
+    //qWarning() << "### SYSP" << m_systemProperties;
+    //qWarning() << "### GL  " << m_openGLConfiguration;
+
+    // sanity checks
+    if (m_baseDir == qL1S("/"))
+        throw Exception("Runtime launcher received an empty baseDir");
+    if (loggingConfig.isEmpty())
+        throw Exception("Runtime launcher received no logging configuration");
+    if (dbusConfig.isEmpty())
+        throw Exception("Runtime launcher received no D-Bus configuration");
 }
 
 void LauncherMain::setupDBusConnections() Q_DECL_NOEXCEPT_EXPR(false)

src/launcher-lib/launchermain.h
@@ -64,10 +64,11 @@ class LauncherMain : public LauncherMainBase, public SharedMain
 {
     Q_OBJECT
 public:
-    LauncherMain(int &argc, char **argv, const QByteArray &configYaml = QByteArray()) Q_DECL_NOEXCEPT_EXPR(false);
+    LauncherMain(int &argc, char **argv) Q_DECL_NOEXCEPT;
     ~LauncherMain();
 
 public:
+    void loadConfiguration(const QByteArray &configYaml = QByteArray()) Q_DECL_NOEXCEPT_EXPR(false);
     void setupDBusConnections() Q_DECL_NOEXCEPT_EXPR(false);
 
     QString baseDir() const;

commit becfc7759c61c0a5fe83e63342727ed61100ffad

Fix regression introduced in d47e6e66
QML Apps would not default to the AM's logging and OpenGL settings anymore.

Change-Id: I0fb89128eab9c4a0964e8bf95ddd250ba20d9662
Reviewed-by: Dominik Holland <dominik.holland@pelagicore.com>

src/launchers/qml/main.cpp
@@ -177,6 +177,12 @@ int main(int argc, char *argv[])
         clp.addOption({ qSL("directload") , qSL("The info.yaml to start."), qSL("info.yaml") });
         clp.process(a);
 
+        bool quicklaunched = clp.isSet(qSL("quicklaunch"));
+        QString directLoad = clp.value(qSL("directload"));
+
+        if (directLoad.isEmpty())
+            a.loadConfiguration();
+
         CrashHandler::setCrashActionConfiguration(a.runtimeConfiguration().value(qSL("crashAction")).toMap());
         a.setupLoggingRules(false, a.loggingRules()); // the verbose flag has already been factored into the rules
         a.setupQmlDebugging(clp.isSet(qSL("qml-debug")));
@@ -185,15 +191,12 @@ int main(int argc, char *argv[])
 
         StartupTimer::instance()->checkpoint("after basic initialization");
 
-        bool quicklaunched = clp.isSet(qSL("quicklaunch"));
-        QString directLoad = clp.value(qSL("directload"));
         if (!directLoad.isEmpty()) {
             QFileInfo fi(directLoad);
             if (!fi.exists() || fi.fileName() != qSL("info.yaml"))
                 throw Exception("--directload needs a valid info.yaml file as parameter");
             directLoad = fi.absoluteFilePath();
         } else {
-            a.loadConfiguration();
             a.setupDBusConnections();
             StartupTimer::instance()->checkpoint("after dbus initialization");
         }
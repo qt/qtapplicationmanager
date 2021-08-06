6d7c33eacfc1d901c096fb48163c427c13bf7c17
Robert Griebl <robert.griebl@qt.io>
2021-08-06 02:09:42 +0200

    Actually set the Application state to NotRunning on app exit
    The Application::state is coupled to the AbstractRuntime::state,
    but if we sever this connection too early, the Application::state
    will stay at ShuttingDown instead of ending in NotRunning.
    
    Change-Id: I541a37bc049b987c92ba2a77c3eb2c20ceea7dac
    Pick-to: 5.15
    Reviewed-by: Bernd Weimer <bernd.weimer@qt.io>

--- src/manager-lib/nativeruntime.cpp
+++ src/manager-lib/nativeruntime.cpp
@@ -232,10 +232,10 @@ void NativeRuntime::shutdown(int exitCode, Am::ExitStatus status)
 
     emit finished(exitCode, status);
 
+    setState(Am::NotRunning);
     if (m_app)
         m_app->setCurrentRuntime(nullptr);
 
-    setState(Am::NotRunning);
     deleteLater();
 }
 

c8418f534261d01745240e89d8eac6104f0efe91
Robert Griebl <robert.griebl@qt.io>
2021-08-19 17:29:10 +0200

    cmake: fix the --force-{single,multi}-process config options
    Change-Id: I2afeffc419627abd87eacc9f27d7dfa0aa8e79e7
    Reviewed-by: Dominik Holland <dominik.holland@qt.io>

--- src/common-lib/qt_cmdline.cmake
+++ src/common-lib/qt_cmdline.cmake
@@ -1,5 +1,5 @@
-qt_commandline_option(force-single-process TYPE enum NAME force_mode VALUE single)
-qt_commandline_option(force-multi-process TYPE enum NAME force_mode VALUE multi)
+qt_commandline_option(force-single-process TYPE void NAME force_mode VALUE single)
+qt_commandline_option(force-multi-process TYPE void NAME force_mode VALUE multi)
 qt_commandline_option(installer TYPE boolean)
 qt_commandline_option(external-dbus-interfaces TYPE boolean)
 qt_commandline_option(tools-only TYPE boolean)

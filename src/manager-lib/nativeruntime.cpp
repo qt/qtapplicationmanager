d696302e9d521c93802447a9c000e062ed87de42
Bernd Weimer <bernd.weimer@qt.io>
2021-09-09 10:24:22 +0200

    Fix immediate application restart
    A crashing application in the application-features example caused the
    entire System UI to crash.
    
    This reverts commit 6d7c33ea and part of f7506f7b.
    
    Change-Id: Idf9cafb15b5e868b7c665269574474aec8a74bf3
    Reviewed-by: Robert Griebl <robert.griebl@qt.io>
    (cherry picked from commit 9211d30bb6639cf46d8d4aa981751ec11996a214)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/manager-lib/nativeruntime.cpp
+++ src/manager-lib/nativeruntime.cpp
@@ -232,9 +232,9 @@ void NativeRuntime::shutdown(int exitCode, Am::ExitStatus status)
 
     emit finished(exitCode, status);
 
-    setState(Am::NotRunning);
     if (m_app)
         m_app->setCurrentRuntime(nullptr);
+    setState(Am::NotRunning);
 
     deleteLater();
 }

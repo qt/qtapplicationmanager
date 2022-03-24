938f204dcbf8ef1b2e653a07db415e1ba86c941c
Mårten Nordheim <marten.nordheim@qt.io>
2022-03-24 10:59:47 +0100

    Update uses of QCryptographicHash::addData to use qsizetype
    Change-Id: Ic1ec93c1864ab88b24014f573bca5f7aa68284b4
    Reviewed-by: Robert Griebl <robert.griebl@qt.io>
    (cherry picked from commit 5b6bfaf371dc1f53750453904039db91aeca8d09)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/package-lib/packageextractor.cpp
+++ src/package-lib/packageextractor.cpp
@@ -376,7 +376,7 @@ void PackageExtractorPrivate::extract()
 
                     switch (packageEntryType) {
                     case PackageEntry_File:
-                        digest.addData(buffer, int(bytesRead));
+                        digest.addData(buffer, qsizetype(bytesRead));
 
                         if (!f.write(buffer, bytesRead))
                             throw Exception(f, "could not write to file");

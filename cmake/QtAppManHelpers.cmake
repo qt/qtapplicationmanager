e14b1081e247503e8f538a862e4e988ddcf22068
Robert Griebl <robert.griebl@qt.io>
2022-05-17 13:44:33 +0200

    Adapt the DBus adaptor generation to the latest changes in qtbase
    Task-number: QTBUG-103521
    Change-Id: Id01ade352309f457f355efff231e0103a1c63312
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>

--- cmake/QtAppManHelpers.cmake
+++ cmake/QtAppManHelpers.cmake
@@ -24,7 +24,7 @@ function(qtam_internal_add_dbus_adaptor target)
             string(SUBSTRING "${file_ext}" 1 -1 file_name) # cut of leading '.'
         endif()
         string(TOLOWER "${file_name}" file_name)
-        set(file_name "${file_name}_adaptor.cpp")
+        set(file_name "${CMAKE_CURRENT_BINARY_DIR}/${file_name}_adaptor.cpp")
 
         # remove the .cpp file from SOURCES
         get_target_property(srcs ${target} SOURCES)

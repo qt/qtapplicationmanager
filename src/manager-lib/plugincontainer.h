f5080ef27740a577692a630ee08238d7b00c1b8e
Robert Griebl <robert.griebl@qt.io>
2023-07-04 16:58:43 +0200

    container-plugins: fix memory leak on container stop
    Change-Id: I1525dafed2929e7aae42857b16105096952e3cfe
    Reviewed-by: Dominik Holland <dominik.holland@qt.io>
    (cherry picked from commit 7e176458bf7c591a59c93afa6213d9d6f671e5f9)

--- src/manager-lib/plugincontainer.h
+++ src/manager-lib/plugincontainer.h
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#include <memory>
 #include <QtAppManManager/abstractcontainer.h>
 #include <QtAppManPluginInterfaces/containerinterface.h>
 
@@ -72,7 +73,7 @@ public:
 
 protected:
     explicit PluginContainer(AbstractContainerManager *manager, Application *app, ContainerInterface *containerInterface);
-    ContainerInterface *m_interface;
+    std::unique_ptr<ContainerInterface> m_interface;
     PluginContainerProcess *m_process;
     bool m_startCalled = false;
 

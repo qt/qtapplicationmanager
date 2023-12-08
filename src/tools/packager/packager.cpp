f828b199361d618c9e94233c50d46aa2273d2c37
Tarja Sundqvist <tarja.sundqvist@qt.io>
2023-12-08 15:40:55 +0200

    Update commercial SPDX-License-Identifier
    Some files in tqtc-qtapplicationmanager were missing the commercial
    SPDX-License-Identifier. Build files, examples, tests, or
    documentation are not updated.
    
    Task-number: QTQAINFRA-5900
    Pick-to: 6.5
    Change-Id: Ie32c81f11a02b8113f82cce5a94c830f96b8799a
    Reviewed-by: Robert Griebl <robert.griebl@qt.io>

--- src/tools/packager/packager.cpp
+++ src/tools/packager/packager.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // Copyright (C) 2019 Luxoft Sweden AB
 // Copyright (C) 2018 Pelagicore AG
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial WITH Qt-GPL-exception-1.0
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #include <memory>
 #include <stdio.h>

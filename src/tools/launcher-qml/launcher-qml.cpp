qt/qtapplicationmanager

commit 2ed714b8cd2dde45f72b1f44f3b70c3bdeeac2e6

Refactor the representation of the YAML configuration
This is a bit of a kitchen sink commit for a lot of things touching
the YAML parsing code. Splitting it up would have been a nightmare
though and touched a lot of code lines multiple times.

- Adding a new field to the YAML confguration was quite involved:
  1) add it to the parser in loadFromSource()
  2) add it to loadFromCache()
  3) add it to saveToCache() (same order as in step 2)
  4) add it to merge()
  5) bump dataStreamVersion()
  6) add one or more public getter function(s) to Configuration,
     depending on the complexity of the data-type.
     In order to get around this problem, a lot of fields just
     used an un-typed QVariantMap.

  This patch eliminates step 3) and 6)

  loadFromCache() and saveToCache() now use a common serialize()
  function and some template magic to do both tasks.

- The YAML file is now mostly resembled by a publicly accessible
  nested struct of structs: this makes the code (a) more readable
  (as the C++ code matches the YAML structure) and (b) does away with
  the gazillions of getters.

- This was also the perfect time to finally use a real data structure
  for the OpenGL config, replacing the QVariantMap hack.

- While at it, the actual field parsing was made a lot more readble:
  - got rid of the unnecessary YamlParse* lambda arguments
  - added helper parseFoo() functions to make code more concise
  - used the same parseFields(<temp list>) syntax everywhere
  - added specialized parsing for time duration values

Change-Id: Ia811056f7220bf7509af146f69f99c6f7e4b09ea
Reviewed-by: Dominik Holland <dominik.holland@qt.io>

src/tools/launcher-qml/launcher-qml.cpp
@@ -103,7 +103,7 @@ int main(int argc, char *argv[])
         if (directLoadManifest.isEmpty())
             am->loadConfiguration();
 
-        CrashHandler::setCrashActionConfiguration(am->runtimeConfiguration().value(u"crashAction"_s).toMap());
+        //CrashHandler::setCrashActionConfiguration(am->runtimeConfiguration().value(u"crashAction"_s).toMap());
         // the verbose flag has already been factored into the rules:
         am->setupLogging(false, am->loggingRules(), QString(), am->useAMConsoleLogger());
         am->setupQmlDebugging(clp.isSet(u"qml-debug"_s));

doc/configuration.qdoc
@@ -10,7 +10,7 @@
 \ingroup qtappman-highlights
 \title Configuration
 \brief Configure the application manager through configuration (config) files and the command line.
-\section1 Main Configuration
+\section1 Introduction
 
 You can configure the application manager through configuration (config) files and the command line. To display the
 currently supported command line options, run \c{appman --help}. Some of these options can also be
@@ -19,6 +19,8 @@ option.
 
 The config file is a \l{YAML syntax}{YAML file that follows the same rules} as all the other YAML
 files used in the application manager.
+
+\section3 Variable Substitution
 
 Before parsing the YAML document, the application manager substitutes variables denoted by a
 syntax similar to \c bash: \c ${VariableName}. There are different types of variables that can be
@@ -45,6 +47,8 @@ substituted this way:
             otherwise by your system administrator.
 \endtable
 
+\section3 Multiple Values
+
 If an option is specified in more than one place: either on the command line and the config files,
 or across multiple config files, the final value is resolved based on these rules:
 
@@ -58,8 +62,20 @@ or across multiple config files, the final value is resolved based on these rule
 \c ui/importPaths. In this case, all values, regardless of where they are specified, are
 merged into one single, final list.
 
+\section3 Time duration values
+\keyword time-duration
+Starting with version 6.8, all time duration values should be specified with a unit suffix. The
+following units are supported: \c h, \c min, \c s, \c ms and \c us.
+Fields that have existed before 6.8 still accept values without a unit suffix, but it is recommended
+to upgrade all time durations to use the new syntax.
+In addition, a zero time duration can also be specified as \c off.
+
+\section3 Example
+
 A very minimalistic \c am-config.yaml example would be something like this, which defines the
 main qml entry point and two import paths:
+
+\section1 Main Configuration
 
 \badcode
 formatVersion: 1
@@ -369,14 +385,14 @@ ui:
         \li [\c logging/useAMConsoleLogger]
         \li bool
         \li Always use the application manager specific logging function, which enables colored
-            console output. If no value or an invalid value is provided, the logging function is
-            only used when messagePattern isn't set.
+            console output. If any non-boolean value is provided (preferably the string \c auto),
+            the logging function is only used when \c messagePattern isn't set. (default: \c auto)
     \row
         \li [\c logging/dlt/id]
         \li string
         \li If provided, it is used as the automotive DLT application ID. The size is limited to
             four characters, additional characters are discarded.
-            \note The default ID, "PCAM", is used until this configuration option has been applied.
+            \note The default ID, "QTAM", is used until this configuration option has been applied.
     \row
         \li [\c logging/dlt/description]
         \li string
@@ -459,8 +475,9 @@ ui:
             (default: 5)
     \row
         \li [\c quicklaunch/failedStartLimitIntervalSec]
-        \li int
-        \li See \c failedStartLimit above. (default: 10)
+        \li duration
+        \li A \l{time-duration}{time interval} with seconds precision, see \c failedStartLimit above.
+            (default: 10s)
     \row
         \li \b --wayland-socket-name
             \br [\c wayland/socketName]
@@ -730,10 +747,10 @@ The following conditions are handled:
             your kernel configuration. (default: true)
     \row
         \li \c waitForGdbAttach
-        \li int
-        \li Specifies a timeout in seconds while the crashed program is being held in the stopped
-            state, waiting for a debugger to attach. Any value \c{<= 0} skips this step.
-            (default: 0)
+        \li duration
+        \li Specifies a \l{time-duration}{time duration} with seconds precision: it is the time a
+            crashed program is being held in the stopped state, waiting for a debugger to attach.
+            Any value \c{<= 0} skips this step. (default: 0s)
     \row
         \li \c stackFramesToIgnore/onCrash
         \li int
@@ -753,10 +770,10 @@ The following conditions are handled:
 
 \section1 Intent Timeout Specification
 
-The \c intents/timeouts sub-object lets you specify the exact timeouts in milli-seconds for the
-asynchronous IPC used to implement the intents mechanism. Any intent whose lifetime exceeds a
-timeout for the stages shown below is canceled by the system and an error reply is sent back
-to the original sender.
+The \c intents/timeouts sub-object lets you specify the exact \l{time-duration}{timeouts} with
+milli-seconds precision for the asynchronous IPC used to implement the intents mechanism. Any
+intent whose lifetime exceeds a timeout for the stages shown below is canceled by the system and
+an error reply is sent back to the original sender.
 Setting any of the timeout values to \c 0 or a negative number disables the timeout handling for
 that specific stage.
 
@@ -767,25 +784,25 @@ that specific stage.
         \li Description
     \row
         \li \c disambiguation
-        \li int
-        \li The time interval in milli-seconds, in which a disambiguation decision for an intent
-            has to be made in the System UI. (default: 10000)
+        \li duration
+        \li The time interval in which a disambiguation decision for an intent
+            has to be made in the System UI. (default: 10s)
     \row
         \li \c startApplication
-        \li int
-        \li The time interval in milli-seconds, in which an application that should handle an
+        \li duration
+        \li The time interval in which an application that should handle an
             intent request is supposed to be started - of course, only if it is not already
-            running. (default: 3000)
+            running. (default: 3s)
     \row
         \li \c replyFromApplication
-        \li int
-        \li The time interval in milli-seconds, in which the handling application needs to
+        \li duration
+        \li The time interval in which the handling application needs to
             acknowledge the receipt of an intent request. For QML applications, this is done in
-            IntentHandler::requestReceived. (default: 5000)
+            IntentHandler::requestReceived. (default: 5s)
     \row
         \li \c replyFromSystem
-        \li int
-        \li The time interval in milli-seconds, after which the complete handling of a single
+        \li duration
+        \li The time interval after which the complete handling of a single
             intent needs to be finished: this starts when actually creating the intent in the
             requesting application, going through the server to the handling application and then
             returning the (error) result back from the handling to the requesting application.
@@ -794,7 +811,7 @@ that specific stage.
                   sub-steps towards the completion of an intent request, while this timeout
                   represents the whole process. Making it slightly bigger than the sum accounts
                   for processing and communication overhead that is not part of the individual sub
-                  timeouts. (default: 20000)
+                  timeouts. (default: 20s)
 \endtable
 
 Please be aware that due to the asynchronous nature of the intent delivery, an intent might be

examples/applicationmanager/custom-appman/custom-appman.cpp
@@ -34,7 +34,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 
         a->setup(cfg.get());
         a->loadQml();
-        a->showWindow(cfg->fullscreen() && !cfg->noFullscreen());
+        a->showWindow();
 
     } catch (const std::exception &e) {
         qCritical() << "ERROR:" << e.what();

src/application-lib/applicationinfo.cpp
@@ -59,7 +59,7 @@ QVariantMap ApplicationInfo::allAppProperties() const
 
 quint32 ApplicationInfo::dataStreamVersion()
 {
-    return 4;
+    return 5;
 }
 
 void ApplicationInfo::writeToDataStream(QDataStream &ds) const
@@ -75,8 +75,11 @@ void ApplicationInfo::writeToDataStream(QDataStream &ds) const
        << m_runtimeParameters
        << m_supportsApplicationInterface
        << m_capabilities
-       << m_openGLConfiguration
-       << m_dltConfiguration
+       << m_openGLConfiguration.desktopProfile
+       << m_openGLConfiguration.esMajorVersion
+       << m_openGLConfiguration.esMinorVersion
+       << m_dltId
+       << m_dltDescription
        << m_supportedMimeTypes
        << m_categories
        << m_names
@@ -99,8 +102,11 @@ ApplicationInfo *ApplicationInfo::readFromDataStream(PackageInfo *pkg, QDataStre
        >> app->m_runtimeParameters
        >> app->m_supportsApplicationInterface
        >> app->m_capabilities
-       >> app->m_openGLConfiguration
-       >> app->m_dltConfiguration
+       >> app->m_openGLConfiguration.desktopProfile
+       >> app->m_openGLConfiguration.esMajorVersion
+       >> app->m_openGLConfiguration.esMinorVersion
+       >> app->m_dltId
+       >> app->m_dltDescription
        >> app->m_supportedMimeTypes
        >> app->m_categories
        >> app->m_names
@@ -147,7 +153,10 @@ QVariantMap ApplicationInfo::toVariantMap() const
     map[u"manifestDir"_s] = map[u"baseDir"_s]; // 5.12 backward compatibility
     map[u"installationLocationId"_s] = packageInfo()->installationReport() ? u"internal-0"_s : QString();
     map[u"supportsApplicationInterface"_s] = m_supportsApplicationInterface;
-    map[u"dlt"_s] = m_dltConfiguration;
+    QVariantMap dltMap;
+    dltMap[u"id"_s] = m_dltId;
+    dltMap[u"description"_s] = m_dltDescription;
+    map[u"dlt"_s] = dltMap;
 
     return map;
 }
@@ -187,7 +196,7 @@ QString ApplicationInfo::documentUrl() const
     return m_documentUrl;
 }
 
-QVariantMap ApplicationInfo::openGLConfiguration() const
+OpenGLConfiguration ApplicationInfo::openGLConfiguration() const
 {
     return m_openGLConfiguration;
 }
@@ -197,9 +206,14 @@ bool ApplicationInfo::supportsApplicationInterface() const
     return m_supportsApplicationInterface;
 }
 
-QVariantMap ApplicationInfo::dltConfiguration() const
+QString ApplicationInfo::dltId() const
 {
-    return m_dltConfiguration;
+    return m_dltId;
+}
+
+QString ApplicationInfo::dltDescription() const
+{
+    return m_dltDescription;
 }
 
 QStringList ApplicationInfo::categories() const

src/application-lib/applicationinfo.h
@@ -14,6 +14,7 @@
 #include <QtCore/QVector>
 
 #include <QtAppManCommon/global.h>
+#include <QtAppManCommon/openglconfiguration.h>
 
 QT_FORWARD_DECLARE_CLASS(QDataStream)
 
@@ -43,9 +44,10 @@ public:
     QStringList capabilities() const;
     QStringList supportedMimeTypes() const;
     QString documentUrl() const;
-    QVariantMap openGLConfiguration() const;
+    OpenGLConfiguration openGLConfiguration() const;
     bool supportsApplicationInterface() const;
-    QVariantMap dltConfiguration() const;
+    QString dltId() const;
+    QString dltDescription() const;
 
     QStringList categories() const;
 
@@ -73,10 +75,11 @@ private:
     QVariantMap m_runtimeParameters;
     bool m_supportsApplicationInterface = false;
     QStringList m_capabilities;
-    QVariantMap m_openGLConfiguration;
+    OpenGLConfiguration m_openGLConfiguration;
     QStringList m_supportedMimeTypes; // deprecated
     QString m_documentUrl; // deprecated
-    QVariantMap m_dltConfiguration;
+    QString m_dltId;
+    QString m_dltDescription;
 
     QStringList m_categories;
     QMap<QString, QString> m_names; // language -> name

src/application-main-lib/applicationmain.cpp
@@ -142,7 +142,7 @@ QString ApplicationMain::notificationDBusName() const
     return m_dbusAddressNotifications.isEmpty() ? QString() : u"am_notification_bus"_s;
 }
 
-QVariantMap ApplicationMain::openGLConfiguration() const
+OpenGLConfiguration ApplicationMain::openGLConfiguration() const
 {
     return m_openGLConfiguration;
 }
@@ -271,7 +271,7 @@ void ApplicationMain::loadConfiguration(const QByteArray &configYaml) noexcept(f
 
     QVariantMap uiConfig = m_configuration.value(u"ui"_s).toMap();
     m_slowAnimations = uiConfig.value(u"slowAnimations"_s).toBool();
-    m_openGLConfiguration = uiConfig.value(u"opengl"_s).toMap();
+    m_openGLConfiguration = OpenGLConfiguration::fromMap(uiConfig.value(u"opengl"_s).toMap());
     m_iconThemeName = uiConfig.value(u"iconThemeName"_s).toString();
     m_iconThemeSearchPaths = uiConfig.value(u"iconThemeSearchPaths"_s).toStringList();
 

src/application-main-lib/applicationmain.h
@@ -10,6 +10,7 @@
 #include <QtCore/QPointer>
 #include <QtCore/QVector>
 #include <QtAppManCommon/global.h>
+#include <QtAppManCommon/openglconfiguration.h>
 #include <QtAppManSharedMain/sharedmain.h>
 #if QT_CONFIG(am_widgets_support)
 #  include <QtWidgets/QApplication>
@@ -61,7 +62,7 @@ public:
     QStringList loggingRules() const;
     QVariant useAMConsoleLogger() const;
     QString dltLongMessageBehavior() const;
-    QVariantMap openGLConfiguration() const;
+    OpenGLConfiguration openGLConfiguration() const;
     QString iconThemeName() const;
     QStringList iconThemeSearchPaths() const;
 
@@ -118,7 +119,7 @@ private:
     QByteArray m_securityToken;
 
     bool m_slowAnimations = false;
-    QVariantMap m_openGLConfiguration;
+    OpenGLConfiguration m_openGLConfiguration;
     QString m_iconThemeName;
     QStringList m_iconThemeSearchPaths;
 

commit 5599e51a2155bd23f12abcb5c9d605360a0f334e

Fix crashAction configuration for the QML runtime not applying
Commit 2ed714b8cd2dde45f72b1f44f3b70c3bdeeac2e6 accidentally broke
custom crashAction configurations for the QML runtime.

Change-Id: Idb8660e6cdeaa53ae5351b381b40b9a65eea79dc
Pick-to: 6.8
Reviewed-by: Dominik Holland <dominik.holland@qt.io>

src/tools/launcher-qml/launcher-qml.cpp
@@ -105,7 +105,14 @@ int main(int argc, char *argv[])
         if (directLoadManifest.isEmpty())
             am->loadConfiguration();
 
-        //CrashHandler::setCrashActionConfiguration(am->runtimeConfiguration().value(u"crashAction"_s).toMap());
+        const QVariantMap ca = am->runtimeConfiguration().value(u"crashAction"_s).toMap();
+        const QVariantMap ca_sfti = ca.value(u"stackFramesToIgnore"_s).toMap();
+        CrashHandler::setCrashActionConfiguration(ca.value(u"printBacktrace"_s, true).toBool(),
+                                                  ca.value(u"printQmlStack"_s, true).toBool(),
+                                                  ca.value(u"waitForGdbAttach"_s, 0).toInt(),
+                                                  ca.value(u"dumpCore"_s, true).toBool(),
+                                                  ca_sfti.value(u"onCrash"_s, -1).toInt(),
+                                                  ca_sfti.value(u"onException"_s, -1).toInt());
         // the verbose flag has already been factored into the rules:
         am->setupLogging(false, am->loggingRules(), QString(), am->useAMConsoleLogger());
         am->setupQmlDebugging(clp.isSet(u"qml-debug"_s));
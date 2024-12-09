5d615e0104166381ae26adde5f858c626ee2c71a
Robert Griebl <robert.griebl@qt.io>
2024-12-09 13:21:34 +0100

    Fix resource leak in ProcessStatus
    Due to a broken lambda capture, all the /proc/smaps file handles were
    leaking.
    
    Change-Id: I2493f5d809e63f882d1c1f1c8a60b66b30d4c481
    Pick-to: 6.8 6.9
    Reviewed-by: Dominik Holland <dominik.holland@qt.io>

--- src/monitor-lib/processreader.cpp
+++ src/monitor-lib/processreader.cpp
@@ -111,7 +111,7 @@ static uint parseValue(const char *pl) {
 bool ProcessReader::readSmaps(const QByteArray &smapsFile, Memory &mem)
 {
     FILE *sf = nullptr;
-    auto closeFile = qScopeGuard([=]() { if (sf) fclose(sf); });
+    auto closeFile = qScopeGuard([&]() { if (sf) fclose(sf); });
 
     sf = fopen(smapsFile.constData(), "r");
     if (!sf)

e836cc1fbc31c91bcc61c221959499eac1699595
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-01-02 13:12:05 +0200

    Revert "Update commercial SPDX-License-Identifier"
    This reverts commit 5fe758838515ac6c06c98ccc0593efc6eb50dcae.
    
    Revert of commercial SPDX license identifiers is required for the
    Qt 6.5.x opensource releases, Qt 6.5.4 onwards.
    
    Change-Id: I84dc919f1f26eaa4efa732daadc7a91e2ab70e49
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/dbus-lib/abstractdbuscontextadaptor.cpp
+++ src/dbus-lib/abstractdbuscontextadaptor.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // Copyright (C) 2019 Luxoft Sweden AB
 // Copyright (C) 2018 Pelagicore AG
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
 #include <QDBusAbstractAdaptor>
 

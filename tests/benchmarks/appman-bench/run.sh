6fd05449d8e4732ab2287c723b79a65d6fb0f41c
Lucie Gérard <lucie.gerard@qt.io>
2025-03-05 15:08:22 +0100

    Update license check
    The module is now:
    - checked against quip18 [1] rules (licenseRule.json)
    - reuse compliant (REUSE.toml)
    - ready for source SBOM check
    
    [1] https://contribute.qt-project.org/quips/18
    
    Task-number: QTBUG-131434
    Pick-to: 6.8
    Change-Id: If6acf1a8d7ea20d9419f1a4a782b4ab2ae1b0c40
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit 5bbbb0149b30c491b0153429408ea6cccae00e67)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/benchmarks/appman-bench/run.sh
+++ tests/benchmarks/appman-bench/run.sh
@@ -2,7 +2,7 @@
 # Copyright (C) 2021 The Qt Company Ltd.
 # Copyright (C) 2019 Luxoft Sweden AB
 # Copyright (C) 2018 Pelagicore AG
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 
 SCRIPT=$(dirname $0)

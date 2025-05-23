a2f38152a0ef01437886ba139d51c496c132fb20
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-05-23 07:58:20 +0300

    Update commercial SPDX-License-Identifier
    Updated the commercial SPDX-License-Identifier to
    the source files. Examples, tests, documentation,
    or build files are not updated.
    
    Modified licenseRule.json and REUSE.toml ccordingly.
    
    Task-number: QTBUG-136484
    Change-Id: I702411183e64d96eecf2a9b5bbd32fae37d5b74c
    Reviewed-by: Robert Griebl <robert.griebl@qt.io>

--- src/package-lib/packageutilities_p.h
+++ src/package-lib/packageutilities_p.h
@@ -1,7 +1,7 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // Copyright (C) 2019 Luxoft Sweden AB
 // Copyright (C) 2018 Pelagicore AG
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #ifndef PACKAGEUTILITIES_P_H
 #define PACKAGEUTILITIES_P_H
--- src/tools/packager/packagingjob.h
+++ src/tools/packager/packagingjob.h
@@ -1,7 +1,7 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // Copyright (C) 2019 Luxoft Sweden AB
 // Copyright (C) 2018 Pelagicore AG
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #ifndef PACKAGINGJOB_H
 #define PACKAGINGJOB_H

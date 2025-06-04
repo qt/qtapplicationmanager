qt/qtapplicationmanager

commit 4e00df28c253f3a92cd4d17a464418261f54c602

Minimize Wayland window size changes on app startup
Make sure to only ever send out one Wayland configure request to the
client on startup to avoid re-allocating buffers.

Change-Id: Id5d46ae4bfa056d458242e913abe046de86e925a
Pick-to: 6.9 6.8
Reviewed-by: Dominik Holland <dominik.holland@qt.io>

src/window-lib/windowitem.cpp
@@ -321,6 +321,8 @@ void WindowItem::componentComplete()
 {
     ensureCurrentContextIsSystemUI(this);
     QQuickFocusScope::componentComplete();
+    if (m_impl)
+        m_impl->componentComplete();
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -494,6 +496,14 @@ QQuickItem *WindowItem::WaylandImpl::backingItem()
     return m_waylandItem;
 }
 
+void WindowItem::WaylandImpl::componentComplete()
+{
+    m_complete = true;
+    QSizeF s { q->width(), q->height() };
+    if (s.isValid())
+        updateSize(s);
+}
+
 void WindowItem::WaylandImpl::tearDown()
 {
     m_waylandItem->setSurface(nullptr);
@@ -504,7 +514,7 @@ void WindowItem::WaylandImpl::updateSize(const QSizeF &newSize)
 {
     m_waylandItem->setSize(newSize);
 
-    if (q->primary() && q->m_objectFollowsItemSize)
+    if (q->primary() && q->m_objectFollowsItemSize && m_complete)
         m_waylandWindow->resize(newSize.toSize());
 }
 

src/window-lib/windowitem.h
@@ -97,6 +97,7 @@ private:
         virtual bool focusOnClick() const = 0;
         virtual void setFocusOnClick(bool focusOnClick) = 0;
         virtual QQuickItem *backingItem() = 0;
+        virtual void componentComplete() {};
         WindowItem *q;
 
         Q_DISABLE_COPY_MOVE(Impl)
@@ -138,9 +139,11 @@ private:
         bool focusOnClick() const override;
         void setFocusOnClick(bool focusOnClick) override;
         QQuickItem *backingItem() override;
+        void componentComplete() override;
 
         WaylandWindow *m_waylandWindow{nullptr};
         WaylandQuickIgnoreKeyItem *m_waylandItem{nullptr};
+        bool m_complete = false;
     };
 #endif // QT_CONFIG(am_multi_process)
 

commit e02cfbaacf1a2570eb3a9c11018bdf497a49a872

Fix regression in WindowItem.objectFollowsItemSize
Introduced in commit 4e00df28c253f3a92cd4d17a464418261f54c602

This optimization strategy is still valid, but we didn't account for
WindowItems that did not start life with a valid "window" property.
The solution is to keep the "m_completed" flag in the WindowItem
itself, and not in the volatile "m_impl" object.

Change-Id: I20bcbecafc33260a95c116ba5074ed3dd1a7d052
Fixes: QTBUG-137056
Pick-to: 6.10 6.9 6.8
Reviewed-by: Dominik Holland <dominik.holland@qt.io>

src/window-lib/windowitem.cpp
@@ -321,6 +321,8 @@ void WindowItem::componentComplete()
 {
     ensureCurrentContextIsSystemUI(this);
     QQuickFocusScope::componentComplete();
+
+    m_complete = true;
     if (m_impl)
         m_impl->componentComplete();
 }
@@ -498,7 +500,6 @@ QQuickItem *WindowItem::WaylandImpl::backingItem()
 
 void WindowItem::WaylandImpl::componentComplete()
 {
-    m_complete = true;
     QSizeF s { q->width(), q->height() };
     if (s.isValid())
         updateSize(s);
@@ -514,7 +515,7 @@ void WindowItem::WaylandImpl::updateSize(const QSizeF &newSize)
 {
     m_waylandItem->setSize(newSize);
 
-    if (q->primary() && q->m_objectFollowsItemSize && m_complete)
+    if (q->primary() && q->m_objectFollowsItemSize && q->m_complete)
         m_waylandWindow->resize(newSize.toSize());
 }
 

src/window-lib/windowitem.h
@@ -143,12 +143,12 @@ private:
 
         WaylandWindow *m_waylandWindow{nullptr};
         WaylandQuickIgnoreKeyItem *m_waylandItem{nullptr};
-        bool m_complete = false;
     };
 #endif // QT_CONFIG(am_multi_process)
 
-    Impl *m_impl{nullptr};
-    bool m_objectFollowsItemSize{true};
+    Impl *m_impl = nullptr;
+    bool m_objectFollowsItemSize = true;
+    bool m_complete = false;
 
     // The only purpose of this item is to ensure all WindowItem children added by System UI code ends up
     // in front of WindowItem's internal QWaylandQuickItem (or the application's root item in case of
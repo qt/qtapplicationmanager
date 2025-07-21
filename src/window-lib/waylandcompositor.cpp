qt/qtapplicationmanager

commit b932b2618a0912ffc92c6c3f9fc09e3116d37833

Wayland: Make it possible to tag WAYLAND_DEBUG output with app ids
This improves on 14986b7c2f700d15303c5ee425f98943b36f3d73, which was
only tagging the server side in a nested compositor setup.

With this patch you get the actual app-id instead of a generic
'QtAM' tag. You can explicitly enable it via:

WAYLAND_DEBUG=1 # or =server
AM_TAGGED_WAYLAND_DEBUG=1

Change-Id: I0da26b19819899eca16e605b54ea56529899fc15
Pick-to: 6.10 6.9 6.8
Reviewed-by: Bernd Weimer <bernd.weimer@qt.io>

src/window-lib/waylandcompositor.cpp
@@ -107,23 +107,37 @@ void WindowSurface::close()
     }
 }
 
-bool WaylandCompositor::s_nestedLogging = false;
+bool WaylandCompositor::s_taggedLogging = false;
 
 QObject *WaylandCompositor::preConstructor(QObject *parent)
 {
     static bool once = false;
     if (!once) {
-        s_nestedLogging = (qEnvironmentVariable("WAYLAND_DEBUG") == u"1")
-                          && (qApp->platformName().startsWith(u"wayland"));
-        if (s_nestedLogging) {
-            // we are a nested compositor and logging with WAYLAND_DEBUG=1 just creates a mess,
+        const auto waylandDebug = qEnvironmentVariable("WAYLAND_DEBUG");
+        const bool amTaggedWaylandDebug = (qEnvironmentVariableIntValue("AM_TAGGED_WAYLAND_DEBUG") == 1);
+        bool nestedLogging = (waylandDebug == u"1")
+                             && (qApp->platformName().startsWith(u"wayland"));
+
+        if (amTaggedWaylandDebug)
+            s_taggedLogging = true;
+
+        if (nestedLogging && !amTaggedWaylandDebug) {            // we are a nested compositor and logging with WAYLAND_DEBUG=1 just creates a mess,
             // as both the server and client side output is intermixed without any tagging.
-            qunsetenv("WAYLAND_DEBUG");
-            qCWarning(LogWayland) << "The application manager is running as a nested compositor and WAYLAND_DEBUG=1 is set.\n"
-                                     "This would produce a mixed client/server log that is not readable.\n"
-                                     "All server side logs will be prefixed with <QtAM> to make them distinguishable.\n"
-                                     "You can use https://github.com/rgriebl/wlanalyze to analyze these log files.";
+            qCInfo(LogWayland) << "The application manager is running as a nested compositor and WAYLAND_DEBUG=1 is set.\n"
+                                  "This will produce a mixed client/server log that is not readable.\n"
+                                  "You should also set AM_TAGGED_WAYLAND_DEBUG=1 (see documentation).";
         }
+        if (amTaggedWaylandDebug && waylandDebug.isEmpty()) {
+            qCInfo(LogWayland) << "AM_TAGGED_WAYLAND_DEBUG is set but ignored, because WAYLAND_DEBUG is not set.\n";
+        } else if (amTaggedWaylandDebug && ((waylandDebug == u"1") || (waylandDebug == u"server"))) {
+            qCInfo(LogWayland) << "AM_TAGGED_WAYLAND_DEBUG is set. All server side WAYLAND_DEBUG logs are now tagged with\n"
+                                  "an <app-id> to make them distinguishable (see documentation).\n";
+        } else if (amTaggedWaylandDebug && (waylandDebug == u"client")) {
+            qCInfo(LogWayland) << "AM_TAGGED_WAYLAND_DEBUG is set, but this does not affect WAYLAND_DEBUG=client.\n";
+        }
+        // The logs are messy enough as is, if you need client side output, start them via
+        // appman-controller and redirect stderr
+        qunsetenv("WAYLAND_DEBUG");
         once = true;
     }
     return parent;
@@ -187,8 +201,8 @@ WaylandCompositor::~WaylandCompositor()
 
 void WaylandCompositor::setupLogging()
 {
-    // only need custom logging, if we are running nested and want to log everything
-    if (!s_nestedLogging)
+    // only need custom logging, if we need to tag
+    if (!s_taggedLogging)
         return;
 
     // The actual logging code below is mostly a verbatim copy of the code in libwayland-server
@@ -196,10 +210,10 @@ void WaylandCompositor::setupLogging()
     wl_display_add_protocol_logger(display(), [](void *user_data,
                                                  enum wl_protocol_logger_type direction,
                                                  const struct wl_protocol_logger_message *message) {
-        bool send = (direction == WL_PROTOCOL_LOGGER_EVENT);
-        auto target = message->resource ? &message->resource->object : nullptr;
-        if (!target)
+        Q_ASSERT(message->resource);
+        if (!message->resource)
             return;
+        bool send = (direction == WL_PROTOCOL_LOGGER_EVENT);
 
         char *buffer;
         size_t buffer_length;
@@ -213,11 +227,25 @@ void WaylandCompositor::setupLogging()
         // the timestamps generated by the libwayland-client logger.
         unsigned int time = (tp.tv_sec * 1000000L) + (tp.tv_nsec / 1000);
 
-        ::fprintf(f, "<%s> [%7u.%03u] %s%s#%u.%s(",
-                  static_cast<const char *>(user_data),
+        QByteArray clientId;
+        if (auto *client = QWaylandClient::fromWlClient(static_cast<WaylandCompositor *>(user_data), message->resource->client)) {
+            const auto apps = ApplicationManager::instance()->fromProcessId(client->processId());
+            for (const auto *app : apps) {
+                if (!clientId.isEmpty())
+                    clientId += '/';
+                clientId += app->id().toUtf8();
+            }
+            if (clientId.isEmpty())
+                clientId = "pid:" + QByteArray::number(client->processId());
+        } else {
+            clientId = "client:0x" + QByteArray::number(reinterpret_cast<std::uintptr_t>(message->resource->client), 16);
+        }
+
+        ::fprintf(f, "[%7u.%03u] <%s> %s%s#%u.%s(",
                   time / 1000, time % 1000,
+                  clientId.constData(),
                   send ? " -> " : "",
-                  message->resource->object.interface->name, target->id,
+                  message->resource->object.interface->name, message->resource->object.id,
                   message->message->name);
 
         const char *signature = message->message->signature;
@@ -299,7 +327,7 @@ void WaylandCompositor::setupLogging()
             ::fprintf(stderr, "%s", buffer);
             ::free(buffer);
         }
-    }, const_cast<char *>("QtAM"));
+    }, this);
 }
 
 void WaylandCompositor::registerOutputWindow(QQuickWindow* window)

doc/debugging.qdoc
@@ -41,6 +41,11 @@ This is a (incomplete) list of environment variables that influence the logging 
     \li AM_NO_DLT_LOGGING
     \li If set to \c 1, debug output is not redirected to DLT.
 \row
+    \li AM_TAGGED_WAYLAND_DEBUG
+    \li If set to \c 1, all server-side \c WAYLAND_DEBUG messages produced by application manager
+        are tagged with an \e <app-id>.
+        You can use \l{wlanalyze} to conveniently analyze these log files.
+\row
     \li AM_STARTUP_TIMER
     \li If set to \c 1, a startup performance analysis is printed to the console. Anything other
         than \c 1 is interpreted as the name of a file to use, instead of the console. For more

doc/external-pages.qdoc
@@ -21,3 +21,7 @@
     \title Flatpak
 */
 
+/*!
+    \externalpage https://github.com/rgriebl/wlanalyze
+    \title wlanalyze
+*/

src/main-lib/configuration.cpp
@@ -222,23 +222,26 @@ Configuration::Configuration(const QStringList &defaultConfigFilePaths,
     const char *description =
         "In addition to the command line options below, the following environment\n"
         "variables can be set:\n\n"
-        "  AM_STARTUP_TIMER       If set to '1', a startup performance analysis is\n"
+        " AM_STARTUP_TIMER        If set to '1', a startup performance analysis is\n"
         "                         printed to the console. Anything other than '1' is\n"
         "                         interpreted as the name of a file to use, instead\n"
         "                         of the console.\n"
         "\n"
-        "  AM_FORCE_COLOR_OUTPUT  Can be set to 'on' to force color output to the\n"
+        " AM_FORCE_COLOR_OUTPUT   Can be set to 'on' to force color output to the\n"
         "                         console and to 'off' to disable it. Any other value\n"
         "                         enables the default, auto-detection behavior.\n"
         "\n"
-        "  AM_NO_CUSTOM_LOGGING   If set to '1', debug output is not redirected to DLT,\n"
+        " AM_NO_CUSTOM_LOGGING    If set to '1', debug output is not redirected to DLT,\n"
         "                         colorized or nicely formatted.\n"
         "\n"
-        "  AM_NO_DLT_LOGGING      If set to '1', debug output is not redirected to DLT.\n"
+        " AM_NO_DLT_LOGGING       If set to '1', debug output is not redirected to DLT.\n"
         "\n"
-        "  AM_NO_CRASH_HANDLER    If set to '1', no crash handler is installed. Use\n"
+        " AM_NO_CRASH_HANDLER     If set to '1', no crash handler is installed. Use\n"
         "                         this, if the application manager's crash handler is\n"
-        "                         interfering with other debugging tools you are using.\n";
+        "                         interfering with other debugging tools you are using.\n"
+        "\n"
+        " AM_TAGGED_WAYLAND_DEBUG If set to '1', all server-side 'WAYLAND_DEBUG' messages\n"
+        "                         are tagged with an '<app-id>'.\n";
 
     d->clp.setApplicationDescription(u"\n"_s + QCoreApplication::applicationName() + u"\n\n"_s
                                     + (additionalDescription ? (QString::fromLatin1(additionalDescription) + u"\n\n"_s) : QString())

src/window-lib/waylandcompositor.h
@@ -113,7 +113,7 @@ protected:
     void onPopupCreated(QWaylandXdgPopup *popup, QWaylandXdgSurface *xdgSurface);
 
     static QObject *preConstructor(QObject *parent);
-    static bool s_nestedLogging;
+    static bool s_taggedLogging;
     void setupLogging();
 
     QWaylandWlShell *m_wlShell;

commit 316fed61d026c5cc1e22f8f7d03916af731521ef

Wayland: fix regression on WAYLAND_DEBUG not working at all anymore
Commit b932b2618a0912ffc92c6c3f9fc09e3116d37833 accidentally disabled
all WAYLAND_DEBUG output, because we unset the env var too early.

Change-Id: I71c191ef002a449cf032700228ae2519236927f3
Pick-to: 6.10 6.9 6.8
Reviewed-by: Dominik Holland <dominik.holland@qt.io>

src/window-lib/waylandcompositor.cpp
@@ -135,9 +135,6 @@ QObject *WaylandCompositor::preConstructor(QObject *parent)
         } else if (amTaggedWaylandDebug && (waylandDebug == u"client")) {
             qCInfo(LogWayland) << "AM_TAGGED_WAYLAND_DEBUG is set, but this does not affect WAYLAND_DEBUG=client.\n";
         }
-        // The logs are messy enough as is, if you need client side output, start them via
-        // appman-controller and redirect stderr
-        qunsetenv("WAYLAND_DEBUG");
         once = true;
     }
     return parent;
@@ -201,6 +198,10 @@ WaylandCompositor::~WaylandCompositor()
 
 void WaylandCompositor::setupLogging()
 {
+    // The logs are messy enough as is, if you need client side output, start them via
+    // appman-controller and redirect stderr
+    qunsetenv("WAYLAND_DEBUG");
+
     // only need custom logging, if we need to tag
     if (!s_taggedLogging)
         return;
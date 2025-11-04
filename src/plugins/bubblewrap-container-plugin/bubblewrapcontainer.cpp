00b9671aa2ac36022a79bb871214670cad719a5c
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-11-04 13:38:19 +0200

    Revert "Update commercial SPDX-License-Identifier"
    This reverts commit a2f38152a0ef01437886ba139d51c496c132fb20.
    
    The revert is needed for the Qt 6.8 LTS opensource releases.
    They cannot contain commercial-only SPDX license identifiers.
    
    Change-Id: I64d5f498e03dac3d86874e325373249afc6b4c2d
    Reviewed-by: Robert Griebl <robert.griebl@qt.io>

--- src/plugins/bubblewrap-container-plugin/bubblewrapcontainer.cpp
+++ src/plugins/bubblewrap-container-plugin/bubblewrapcontainer.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2023 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 // Qt-Security score:critical reason:execute-external-code
 
 #include <tuple>

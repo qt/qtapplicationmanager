4022534bd857ea37743611b680daef51fbf79b25
Robert Griebl <robert.griebl@qt.io>
2026-06-16 19:06:42 +0200

    Tests: add a test for the developer-certificate workflow
    Change-Id: I141f492a00898c4adf47b65366c42e9aa8021649
    Reviewed-by: Thomas Senyk <thomas.senyk@qt.io>
    (cherry picked from commit 9b54321be6647c15a0264a680cccc2f968f74929)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/devmode.h
+++ tests/auto/devmode.h
@@ -5,28 +5,43 @@
 #define DEVMODE_H
 
 #include <QtTest>
+#include <QFile>
+#include <QDir>
+#include <QStandardPaths>
 #include <packagemanager.h>
 
 QT_USE_NAMESPACE_AM
 
-#if defined(QT_BUILD_INTERNAL)
-Q_AUTOTEST_EXPORT extern void qtam_PackageManager_forceUnlockConfiguration();
-#endif
+using namespace Qt::StringLiterals;
 
+// RAII helper that switches the PackageManager into a given development mode for the duration of a
+// test and restores the previous state afterwards. When a PKCS#12 file is passed, it additionally
+// sets the developer certificate (which only works in DevelopmentMode::Application) and makes sure
+// the persisted development-mode.ini is removed again on destruction.
 class DevMode
 {
 public:
-    DevMode(PackageManager::DevelopmentMode mode, bool allowUnsigned = false)
+    DevMode(PackageManager::DevelopmentMode mode, bool allowUnsigned = false,
+            const QString &pkcs12File = { }, const QByteArray &pkcs12Password = { })
         : m_oldMode(PackageManager::instance()->developmentMode())
+        , m_oldUnsigned(PackageManager::instance()->allowInstallationOfUnsignedPackages())
     {
 #if defined(QT_BUILD_INTERNAL)
         qtam_PackageManager_forceUnlockConfiguration();
         PackageManager::instance()->setDevelopmentMode(mode);
         PackageManager::instance()->setAllowInstallationOfUnsignedPackages(allowUnsigned);
+
+        if (!pkcs12File.isEmpty()) {
+            QFile f(pkcs12File);
+            if (f.open(QIODevice::ReadOnly))
+                PackageManager::instance()->setDeveloperCertificate(f.readAll(), pkcs12Password);
+        }
 #else
         QSKIP("This test requires a developer-build");
         Q_UNUSED(mode)
         Q_UNUSED(allowUnsigned)
+        Q_UNUSED(pkcs12File)
+        Q_UNUSED(pkcs12Password)
         Q_UNUSED(m_oldMode)
         Q_UNUSED(m_oldUnsigned)
 #endif
@@ -34,14 +49,22 @@ public:
     ~DevMode()
     {
 #if defined(QT_BUILD_INTERNAL)
+        // clear any developer certificate we might have set (only possible while in Application mode)
+        if (PackageManager::instance()->developmentMode() == PackageManager::DevelopmentMode::Application)
+            PackageManager::instance()->setDeveloperCertificate({ }, { });
+
         PackageManager::instance()->setDevelopmentMode(m_oldMode);
         PackageManager::instance()->setAllowInstallationOfUnsignedPackages(m_oldUnsigned);
+
+        // get rid of the persisted signature, so the next test starts from a clean state
+        QDir dataDir = QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation);
+        QFile::remove(dataDir.absoluteFilePath(u"development-mode.ini"_s));
 #endif
     }
 
 private:
     PackageManager::DevelopmentMode m_oldMode;
-    bool m_oldUnsigned;
+    bool m_oldUnsigned = false;
 };
 
 #endif // DEVMODE_H
